7552d42909103304a6a5a34b9b3b021a86aae60b
CharlieYin <315099409@qq.com>
2025-08-12 21:52:33 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,6 +8,6 @@ import store from './store';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
-    <RouterProvider router={router} />
+    <RouterProvider router={router}/>
   </Provider>
 );
